c57bcaa0b64482937238cbae1b60446df167d59a
Juan Esteban Gil <juanestebang9@gmail.com>
2018-11-21 18:55:16 -0500

    Inicio>Comienzo desarrollo

--- SuspiciusApp/SuspiciusApp/App.xaml.cs
+++ SuspiciusApp/SuspiciusApp/App.xaml.cs
@@ -1,3 +1,4 @@
+using SuspiciusApp.Views;
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -11,8 +12,8 @@ namespace SuspiciusApp
 		{
 			InitializeComponent();
 
-			MainPage = new MainPage();
-		}
+			MainPage = new NavigationPage(new LoginPage());
+        }
 
 		protected override void OnStart ()
 		{
