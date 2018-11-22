532f4f5d0f80ae554926431ad39b3ce94c7c0ecc
Juan Esteban Gil <juanestebang9@gmail.com>
2018-11-21 19:09:13 -0500

    Modelo login agregado y XAML de login

---
+++ SuspiciusApp/SuspiciusApp/Model/Login.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuspiciusApp.Model
+{
+
+    public class Login
+    {
+        public object[] TBLEVENTO { get; set; }
+        public int ID { get; set; }
+        public string N_user { get; set; }
+        public string Contraseña { get; set; }
+        public string Correo { get; set; }
+        public int Telefono { get; set; }
+    }
+
+}
