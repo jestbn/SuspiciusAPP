aa3e85c5bdee91bda19b74123a9803125751d46b
Juan Esteban Gil <juanestebang9@gmail.com>
2018-11-25 21:34:06 -0500

    Intento estable de login

--- SuspiciusApp/SuspiciusApp/Model/Login.cs
+++ SuspiciusApp/SuspiciusApp/Model/Login.cs
@@ -8,11 +8,11 @@ namespace SuspiciusApp.Model
     public class Login
     {
         //public object[] TBLEVENTO { get; set; }
-        public int ID { get; set; }
+        //public int ID { get; set; }
         public string N_user { get; set; }
         public string Contraseña { get; set; }
-        public string Correo { get; set; }
-        public int Telefono { get; set; }
+        //public string Correo { get; set; }
+        //public int Telefono { get; set; }
     }
 
 }
--- SuspiciusApp/SuspiciusApp/Views/LoginPage.xaml.cs
+++ SuspiciusApp/SuspiciusApp/Views/LoginPage.xaml.cs
@@ -36,8 +36,11 @@ namespace SuspiciusApp.Views
                 return;
             }
 
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("http://suspiciusapi.azurewebsites.net");
+            HttpClient client = new HttpClient
+            {
+                //client.BaseAddress = new Uri("http://suspiciusapi.azurewebsites.net"); Vieja api, generaba problemas
+                BaseAddress = new Uri("https://suspapi.azurewebsites.net")
+            };
 
             var autenticacion = new Login
             {
@@ -48,25 +51,30 @@ namespace SuspiciusApp.Views
             string json = JsonConvert.SerializeObject(autenticacion);
             StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
 
-            var request = await client.PostAsync("/api/tblusers", content);
+            //var request = await client.PostAsync("/api/tblusers", content); vieja api, generaba problemas
+            var request = await client.GetAsync("/api/users", HttpCompletionOption.ResponseContentRead);  //SIEMPRE VA A TRAER SI :)
+            //var request = await client.PostAsync("/api/users", content); desarrollo para autenticacion
+
+
             if (request.IsSuccessStatusCode)
             {
                 var responseJson = await request.Content.ReadAsStringAsync();
                 var respuesta = JsonConvert.DeserializeObject<Respuesta>(responseJson);
 
-                if (respuesta.EsPermitido)
-                {
-                    //await Navigation.PushAsync(new CarteleraPage());
-                    await DisplayAlert("EXITO!", "FELICITACIONES MEN", "Aceptar");
-                }
-                else
-                {
-                    await DisplayAlert("Lo sentimos!", respuesta.Mensaje, "Aceptar");
-                }
+                //if (respuesta.EsPermitido)
+                //{
+                //await Navigation.PushAsync(new CarteleraPage());
+                await Navigation.PushAsync(new ListarPage());
+                //await DisplayAlert("EXITO!", "FELICITACIONES MEN", "Aceptar");
+                //}
+                //else
+                //{
+                    //await DisplayAlert("Lo sentimos!", respuesta.Mensaje, "Aceptar");
+                //}
             }
             else
             {
-                await DisplayAlert("Lo sentimos!", "Ha ocurrido un error de comunicacion", "OK");
+                await DisplayAlert("Lo sentimos!", "Ha ocurrido un error de comunicacion / el usuario no existe :)", "OK");
             }
         }
     }
